7400c25fa7d1b364e7098ddee3d574df1cb3c7c1
Cavan Theiss <cavanrt@gmail.com>
2026-06-05 23:22:08 -0500

    Update RedSquare.cpp

--- src/RedSquare.cpp
+++ src/RedSquare.cpp
@@ -39,7 +39,9 @@ int main(){
         std::cerr << "SDL_ttf initialization failed: " << TTF_GetError() << std::endl;
         return 1;
     }
-    
+
+    TTF_Font* scoreFont = TTF_OpenFont("assets/arial.ttf", 40);
+
     // INITIALIZE AUDIO
 
     if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0){
