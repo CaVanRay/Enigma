14ab654d71dd85a60565ec663858eadf31367758
Cavan Theiss <cavanrt@gmail.com>
2026-06-06 07:42:33 -0500

    Update RedSquare.cpp

--- src/RedSquare.cpp
+++ src/RedSquare.cpp
@@ -158,10 +158,12 @@ int main(){
         
         ghostSquare.x += horizontalVelocity;
         ghostSquare.x = std::max(0, std::min(windowWidth - ghostSquare.w, ghostSquare.x)); // prevent moving out of bounds
-        if (SDL_HasIntersection(&ghostSquare, &bluePlatform)|SDL_HasIntersection(&ghostSquare, &greenPlatform)|SDL_HasIntersection(&ghostSquare, &yellowPlatform)) {
+        if (SDL_HasIntersection(&ghostSquare, &bluePlatform)|SDL_HasIntersection(&ghostSquare, &yellowPlatform)) {
             ghostSquare.x -= horizontalVelocity; // undo movement if colliding with blue platform
         }
-        
+        if (SDL_HasIntersection(&ghostSquare, &greenPlatform){
+            ghostSquare.x -= horizontalVelocity; // undo movement if colliding with green platform
+        }
         // **************************************** VERTICAL MOVEMENT ********************************
         
         
