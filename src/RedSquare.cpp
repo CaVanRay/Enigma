1602d0311ca0ae71b7f7265efdc75e43612e90b4
Cavan Theiss <cavanrt@gmail.com>
2026-06-25 14:25:40 -0500

    Update RedSquare.cpp

--- src/RedSquare.cpp
+++ src/RedSquare.cpp
@@ -111,6 +111,7 @@ int main(){
 
     // FOR SHAKING THE SCREEN WHEN LANDING OR GETTING HIT
     float shakeX = 0.0f;
+    float shakeY = 0.0f;
     
     // SET PLAYABLE AREA AVAILABLE
     const int worldWidth =  windowWidth;
