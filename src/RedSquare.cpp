6f2d7b04ac7857bab76d7e7657f96fdd7b2ba12a
Cavan Theiss <cavanrt@gmail.com>
2026-07-05 17:33:35 -0500

    Update RedSquare.cpp

--- src/RedSquare.cpp
+++ src/RedSquare.cpp
@@ -148,6 +148,7 @@ int main(){
     float verticalVelocity = 0.0f; // Speed of the red square
     const float GRAVITY = 900.0f; // Gravity affecting the red square
     bool onGround = false; // To check if the red square is on the ground for jumping
+    bool isJumping = false;
 
     // **************************************** GAME LOOP ****************************************
     
